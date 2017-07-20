8eab8650827c36f4d7de1cfa47354f70bbd9bc8e
alamdeep-1 <alamdeep.1@gmail.com>
2017-07-21 01:42:38 +0530

    instaclone project

---
+++ myapp/admin.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.contrib import admin
+
+from myapp.models import UserModel,LikeModel,PostModel,CommentModel
+
+admin.site.register(UserModel)
+admin.site.register(LikeModel)
+admin.site.register(PostModel)
+admin.site.register(CommentModel)
+
+from django.contrib import admin
+
+# Register your models here.
